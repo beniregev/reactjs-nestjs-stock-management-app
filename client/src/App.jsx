0b35227728192442c0a7f8b95d74265a719e84c7
binyamin.regev <beniregev@gmail.com>
2025-04-26 21:45:12 +0300

    Initial commit
    Created ReactJS client application using Vite.
    
    Removed the boilerplate code and icons that I will not use.
    
    Added TailwindCSS, PostCSS, MobX, Axios, and Ant design to the client application.
    
    Created a NestJS server application.

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-indigo-600 p-5.5 text-yellow-500 text-3xl font-bold underline rounded-4xl">
+        Vite + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
