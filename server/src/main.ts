b9b961594dfc58ac010d1a85297be5b9ddcbd079
binyamin.regev <beniregev@gmail.com>
2025-04-27 15:24:38 +0300

    Installing Swagger dependency.
    Updating "main.ts" with code for Swagger setup.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { StockService } from './stock/stock.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';   // ✅ Add this!
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Stock Management API')
+    .setDescription('API for managing user stock portfolios')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  
+
+  const stockService = app.get(StockService);
+  // await stockService.populateInitialData();
+  await stockService.populateInitialStocks(); // populate on startup
+
+  await app.listen(3000);
 }
 bootstrap();
